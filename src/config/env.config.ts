6d1745403b165109b17d6a3e1261b751f21ae05a
Felippe <felipperian365@gmail.com>
2024-10-05 16:07:26 -0300

    chore: adjusting env.config

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,18 +1,6 @@
-import path from 'path';
 import z from 'zod';
-import dotenv from 'dotenv';
 
 export default function loadConfig(): void {
-  const envPath = path.join(__dirname, '..', '..', '.env');
-
-  const result = dotenv.config({ path: envPath });
-
-  if (result.error) {
-    throw new Error(
-      `Failed to load .env file from path ${envPath}: ${result.error.message}`,
-    );
-  }
-
   const schema = z
     .object({
       NODE_ENV: z.enum(['development', 'testing', 'production']),
